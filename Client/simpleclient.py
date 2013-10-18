4a42e2e82c498bc44140d36cfc5771659642d24c
eshyong <eric.shyong@gmail.com>
2013-10-17 18:22:46 -0700

    select server and client

--- Client/simpleclient.py
+++ Client/simpleclient.py
@@ -1,32 +1,67 @@
-import socket, getpass
+import getpass
+import select
+import socket
+import sys
 
 class Client():
 	def __init__(self):
+		"""create client socket, get user name"""
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.connected = False
 		self.username = self.authenticate()
 
 	def authenticate(self):
+		"""TODO: authenticate user"""
 		username = getpass.getpass('username: ')
 		password = getpass.getpass('password: ')
 		return username
 
 	def connect(self):
+		"""accepts user input, authenticates, and then connects"""
 		print 'please enter the server and port you wish to connect to.'
 		host = raw_input('server: ')
-		port = raw_input('port: ')
+		port = int(raw_input('port: '))
 		try:
 			self.socket.connect((host, port))
+			self.socket.send(self.username)
 			print 'success!'
+			return True
 		except socket.error:
 			print 'error connecting to server. try again.'
 
+	def run(self):
+		"""normal operation"""
+		# read and send messages from CLI
+		msg = sys.stdin.readline()
+		if msg == 'exit':
+			# quit message is sent to client
+			self.shutdown()
+		else:
+			self.socket.send(msg)
+
+		# receive messages from server
+		data = self.socket.recv(1024)
+
+		# broken connection
+		if not data:
+			self.shutdown()
+		else:
+			print data,
+
+	def shutdown(self):
+		# close socket and exit
+		self.socket.close()
+		sys.exit()
+
 def main():
+	"""client is initialized and connected here"""
 	c = Client()
-	while not c.connected:
-		c.connect()
-	while True:
-		c.recv(1024)
+	connected = False
+	while not connected:
+		connected = c.connect()
+	
+	# operation loop
+	while 1:
+		c.run()
 
 if __name__ == '__main__':
 	main()
---
+++ Client/simpleserver.py
@@ -0,0 +1,133 @@
+import select
+import socket
+import sys
+from collections import deque
+
+host = ''
+port = 8080
+backlog = 5
+
+class Server():
+	def __init__(self):
+		"""initialize the server socket and any data structures 
+		for names"""
+		# messages to send
+		self.messages = deque([])
+
+		# names of each client
+		self.names = {}
+
+		# server socket
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.socket.bind((host, port))
+		self.socket.listen(5)
+
+		print 'server listening...'
+		
+		# inputs to read from, outputs to write to
+		self.inputs = [self.socket, sys.stdin]
+		self.outputs = []
+		self.read, self.write, self.error = select.select(self.inputs, 
+									self.outputs,
+									self.inputs)
+
+	def connect(self):
+		"""accept new connections and add clients"""
+		# register client name
+		conn, addr = self.socket.accept()
+		name = conn.recv(1024)
+
+		# add new client to each one
+		self.names[conn] = name
+		self.inputs.append(conn)
+		self.outputs.append(conn)
+		print 'names:', self.names
+		print 'inputs:', self.inputs
+		print 'outputs:', self.outputs
+
+	def getMessage(self, conn):
+		"""receive messages from each client to send out"""
+		data = conn.recv(1024)
+		if data:
+			# append message to queue, to read and send later
+			self.messages.append((self.names[conn], data))
+		else:
+			# assume dead connection
+			conn.close()
+			self.names.pop(conn)
+			self.inputs.remove(conn)
+			self.outputs.remove(conn)
+
+	def run(self):
+		"""normal operations for server"""
+		# select polls all sockets
+		self.read, self.write, self.error = select.select(self.inputs, 
+									self.outputs, 
+									self.inputs)
+
+		# readables
+		for s in self.read:
+			# if s == sys.stdin:
+			# 	# read from CLI
+			# 	cmd = sys.stdin.readline()
+			# 	if cmd == 'exit\n':
+			# 		self.shutdown()
+			# server socket
+			if s == self.socket:
+				# accept new connections
+				self.connect()
+			# client socket
+			else:
+				# get messages from client
+				self.getMessage(s)
+
+		# writables
+		while self.messages:
+			tup = self.messages.popleft()
+			msg = tup[0] + ' says: ' + tup[1]
+			for conn in self.names.keys():
+				print conn, ' writes:', msg,
+				conn.send(msg)
+
+		# errors
+		for s in self.error:
+			print 'ABORT'
+			s.close()
+
+			# check for socket in other lists
+			if s in self.inputs:
+				self.inputs.remove(s)
+			if s in self.outputs:
+				self.outputs.remove(s)
+
+			# finally, remove socket from error list
+			self.error.remove(s)
+
+	def shutdown(self):
+		"""shutdowns all communications"""
+		# close server socket
+		self.socket.close()
+		self.inputs.remove(self.socket)
+
+		# close client connections
+		for conn in self.names.keys():
+			conn.close()
+
+		# remove from lists
+		while self.names:
+			self.names.popitem()
+		while self.inputs:
+			self.inputs.pop()
+		while self.outputs:
+			self.outputs.pop()
+
+def main():
+	server = Server()
+	running = 1
+	while running:
+		server.run()
+
+if __name__ == "__main__":
+	main()
+
